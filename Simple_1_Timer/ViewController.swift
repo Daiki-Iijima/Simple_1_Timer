180e1089453b89bcf36be2b887107d5bc63fe1fd
Daiki Iijima <daiki.applecreate@gmail.com>
2020-02-13 20:59:48 +0900

    タイマーの「スタート」「ストップ」「リセット」処理追加

--- Simple_1_Timer/ViewController.swift
+++ Simple_1_Timer/ViewController.swift
@@ -10,11 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TimerText: UILabel!
+    
+    @IBAction func OnInputButton(_ sender: Any) {
+        
+        if(timer != nil)
+        {
+            self.timer?.invalidate()
+            timer = nil
+            TimerButton.setTitle(startText, for: .normal)
+        }
+        else
+        {
+            TimerButton.setTitle(stopText, for: .normal)
+            //  タイマーを作成、起動
+            timer = Timer.scheduledTimer(
+                timeInterval: 1,    //  ループの場合 : タイマーの間隔 ループしない場合 : 起動までの時間
+                target: self,       //  メソッドを持つメソッド
+                selector: #selector(ViewController.timerUpdate), // 実行対象のメソッド
+                userInfo: nil,      //  オブジェクトにつける値
+                repeats: true       //  ループするか
+            )
+            
+            
+        }
+        
+        
+    }
+    @IBAction func OnResetButton(_ sender: Any) {
+       self.timer?.invalidate()
+        timer = nil
+        TimerButton.setTitle(startText, for: .normal)
+        time = 0;
+        TimerText.text = String(time)
+    }
+    
+    @IBOutlet weak var TimerButton: UIButton!
+    
+    var timer : Timer?
+    private var time = 0
+    
+    private var startText = "スタート"
+    private var stopText = "ストップ"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @objc func timerUpdate() {
+        
+        time += 1
+        
+        TimerText.text = String(time)
+    }
 
 }
 
