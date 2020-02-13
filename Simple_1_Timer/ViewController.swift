7e6524263fdcc45cd5483268bb3e82f5af879e41
Daiki Iijima <daiki.applecreate@gmail.com>
2020-02-13 12:24:45 +0900

    Initial Commit

---
+++ Simple_1_Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple_1_Timer
+//
+//  Created by 飯島大樹 on 2020/02/13.
+//  Copyright © 2020 Daiki Iijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
